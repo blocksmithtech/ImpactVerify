a1eba83d1a25b74cfdcbee676b7f62794a0fd8b5
tomasmcm <tomasmcm@whitesmith.co>
2023-03-16 19:12:39 +0000

    fetch group snapshot and group members

--- index.tsx
+++ index.tsx
@@ -2,8 +2,66 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Tag, Button, GridContainer, GridRow, GridCol } from '@taikai/rocket-kit';
 import { ZkConnectButton, ZkConnectResponse } from "@sismo-core/zk-connect-react";
+const { useEffect, useState } = React
 
 const App = () => {
+  const [groupSnapshot, setGroupSnapshot] = useState({});
+  const [groupMembers, setGroupMembers] = useState({});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const { dataUrl } = await fetchGroupSnapshot()
+      console.log(dataUrl)
+      await fetchGroupMembers(dataUrl)
+    }
+    const fetchGroupSnapshot = async () => {
+      const query = `
+        query getSnapshot {
+          groupSnapshot(
+            groupId: "0x3572d27296a9718a6e5c3274f7076991",
+          ) {
+            id
+            dataUrl
+            size
+            timestamp
+            valueDistribution {
+              numberOfAccounts
+              value
+            }
+          }
+        }
+      `
+
+      const res = await fetch('https://api.sismo.io', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+        },
+        body: JSON.stringify({ query})
+      });
+      const { data, errors } = await res.json();
+      console.log(data)
+      setGroupSnapshot(data.groupSnapshot)
+      return data.groupSnapshot
+    }
+
+    const fetchGroupMembers = async (dataUrl) => {
+      const res = await fetch(dataUrl, {
+        method: 'GET',
+        headers: {
+          'Accept': 'application/json',
+        }
+      });
+      const data = JSON.parse(await res.text());
+      console.log(data)
+      setGroupMembers(data)
+    }
+
+    fetchData()
+      .catch(console.error);
+  }, []);
+
 
   return (
     <GridContainer>
@@ -18,6 +76,13 @@ const App = () => {
             value="Connect"
             variant="solid"
           />
+        </GridCol>
+        <GridCol>
+          <h3>Group Snapshot</h3>
+          <div style={{ whiteSpace: 'pre-wrap' }}>{ JSON.stringify(groupSnapshot, null, 2) }</div>
+
+          <h3>Group Members</h3>
+          <div style={{ whiteSpace: 'pre-wrap' }}>{ JSON.stringify(groupMembers, null, 2) }</div>
         </GridCol>
         <GridCol>
           <ZkConnectButton 
