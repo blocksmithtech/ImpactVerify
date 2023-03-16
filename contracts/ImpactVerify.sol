7970997c1be7a3b9c9edaab42cc8ae68ef2b78ad
tomasmcm <tomasmcm@whitesmith.co>
2023-03-16 19:13:44 +0000

    add base contract

---
+++ contracts/ImpactVerify.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract AddressVoting {
+    mapping(address => uint256) public addressVotes;
+    mapping(address => bool) public approvedAddresses;
+    mapping(address => bool) public blacklistedAddresses;
+    mapping(address => bool) public registeredAddresses;
+    address[] public addresses;
+
+    event AddressRegistered(address indexed addr);
+    event AddressUpvoted(address indexed addr, uint256 votes);
+    event AddressDownvoted(address indexed addr, uint256 votes);
+    event AddressApproved(address indexed addr);
+    event AddressBlacklisted(address indexed addr);
+
+    function registerAddress() public {
+        require(!registeredAddresses[msg.sender], "Address already registered");
+        addresses.push(msg.sender);
+        registeredAddresses[msg.sender] = true;
+        emit AddressRegistered(msg.sender);
+    }
+
+    function upvoteAddress(address addr) public {
+        require(registeredAddresses[msg.sender], "Address must be registered to vote");
+        require(!approvedAddresses[addr] && !blacklistedAddresses[addr], "Address cannot be approved or blacklisted");
+        addressVotes[addr]++;
+        emit AddressUpvoted(addr, addressVotes[addr]);
+        if (addressVotes[addr] == 5) {
+            approvedAddresses[addr] = true;
+            emit AddressApproved(addr);
+        }
+    }
+
+    function downvoteAddress(address addr) public {
+        require(registeredAddresses[msg.sender], "Address must be registered to vote");
+        require(!approvedAddresses[addr] && !blacklistedAddresses[addr], "Address cannot be approved or blacklisted");
+        addressVotes[addr]--;
+        emit AddressDownvoted(addr, addressVotes[addr]);
+        if (addressVotes[addr] == -5) {
+            blacklistedAddresses[addr] = true;
+            emit AddressBlacklisted(addr);
+        }
+    }
+}
